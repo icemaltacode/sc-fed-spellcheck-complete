5b414fa0657e892b4a2cc9a01aa71f04d8bf7c1a
Keith Vassallo <keith@icemalta.com>
2023-06-28 18:29:25 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import { useId, useState, useEffect, memo, useCallback } from 'react';
 import './App.css';
 
+const apiKey = process.env.REACT_APP_BING_API_KEY;
+const apiEndpoint = process.env.REACT_APP_BING_API_ENDPOINT;
+
+async function fetchSuggestions(text) {
+  if (text === "") return [];
+
+  const params = new URLSearchParams({
+    mkt: 'en-us',
+    mode: 'proof',
+    text: text
+  });
+  const response = await fetch(apiEndpoint + '?' + params, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Ocp-Apim-Subscription-Key': apiKey
+    }
+  });
+  const json = await response.json();
+  return json;
+}
+
+function SpellcheckInput({ setCurrentText }) {
+  const textAreaId = useId();
+  
+  const handleTextChanged = (evt) => {
+    if (evt.code === "Space") {
+      setCurrentText(evt.target.value);
+    }
+  };
+
+  return <>
+    <textarea 
+      spellCheck={false}
+      className="form-control" 
+      rows={5} 
+      id={textAreaId} 
+      onKeyDown={(evt) => handleTextChanged(evt)}  
+     />
+  </>
+}
+
+const SpellcheckOutput = memo(({ checkResult }) => {
+  return (checkResult && checkResult.flaggedTokens && checkResult.flaggedTokens.length > 0 ? <>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th scope="col">Possible Error</th>
+          <th scope="col">Suggestions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {checkResult.flaggedTokens.map((flaggedToken, i) => <tr key={i}>
+          <th scope='row'>{flaggedToken.token}</th>
+          <td>{flaggedToken.suggestions.map(s => s.suggestion).join(', ')}</td>
+        </tr>)}
+      </tbody>
+    </table>
+  </> : null)
+});
+
+function SpellChecker() {
+  const [currentText, setCurrentText] = useState(""); 
+  const [suggestions, setSuggestions] = useState(null);
+
+  useEffect(() => {
+    load();
+
+    async function load() {
+      const res = await fetchSuggestions(currentText);
+      setSuggestions(res);
+    }
+  }, [currentText])
+
+  return <div>
+    <small className="text-muted">Type text below to have it analysed.</small>
+    <SpellcheckInput setCurrentText={setCurrentText} />
+    <SpellcheckOutput checkResult={suggestions}/>
+  </div>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className='container'>
+    <div className='rounded-container'>
+      <h3>SpellChecker</h3>
+      <hr />
+      <SpellChecker />
     </div>
-  );
+  </div>
 }
 
 export default App;
